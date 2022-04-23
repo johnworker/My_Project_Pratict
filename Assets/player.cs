702d4dcc295735b313ede5511c426dd0ea2f49d3
johnworker <besscomdark@gmail.com>
2022-04-23 22:33:08 +0800

    添加內容
    角色移動C#程式添加

--- Assets/player.cs
+++ Assets/player.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
+    // 公開在unity調整參數的C#寫法
+    // public float moveSpeed = -5f;
+
+    // 保持隱私在unity調整參數的C#寫法
+    [SerializeField] float moveSpeed = -5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // transform.Translate(-10,0,0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // 如果(輸入.關鍵鈕(鍵盤.右鍵))
+        if(Input.GetKey(KeyCode.D))
+        {
+            // Time.deltaTime 每秒的間隔時間(為了在每個不同的遊戲上有相同遊戲體驗而做)
+            transform.Translate(moveSpeed*Time.deltaTime, 0, 0);
+        }
+
+        else if(Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-moveSpeed*Time.deltaTime, 0, 0);
+        }
     }
 }
